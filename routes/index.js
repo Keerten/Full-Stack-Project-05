429a1c34f00e37ba71199a7aba8f3bd711e1265d
Biraj Gautam <birajgtm@gmail.com>
2024-02-11 19:06:28 -0500

    / route

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const restaurantRouter = require("./restaurant/index.js");
 const orderRouter = require("./order/index.js");
 const deliveryRouter = require("./delivery/index.js");
 
-router.use("/restaurant", restaurantRouter);
+router.use("/", restaurantRouter);
 router.use("/order", orderRouter);
 router.use("/delivery", deliveryRouter);
 
